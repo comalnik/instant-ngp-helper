5bca4a9e02339973ec5095fe51929a50ea26bda5
comalnik <gostencnikluka@gmail.com>
2022-03-02 20:15:52 +0100

    added run.py

--- main.py
+++ main.py
@@ -14,5 +14,7 @@ if vsqu == str('N'):
     print('Make sure to install desktop development with C++! [press any key to continue]: ')
     os.system ('pause')
     os.system("vs.exe")
+call(["python", "path.py"])
 call(["python", "copy.py"])
 os.system('git clone --recursive https://github.com/nvlabs/instant-ngp' + "&" + 'cd instant-ngp' + "&" + 'C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\\2019\\Community\\Common7\\Tools\\VsDevCmd.bat' + "&" + 'cmake . -B build' + "&" + 'cmake --build build --config RelWithDebInfo -j 16' )
+call(["python", "run.py"])
---
+++ run.py
@@ -0,0 +1,4 @@
+import os
+import tkinter
+path = "/tmp"
+os.mkdir(path)
