86c4b775c8a0f7a4cd0a44f0845115ac18f56e3b
Luka <gostencnikluka@gmail.com>
2022-03-03 11:59:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,3 @@ if vsqu == str('N'):
 call(["python", "copy.py"])
 os.system('git clone --recursive https://github.com/nvlabs/instant-ngp' + "&" + 'cd instant-ngp' + "&" + 'C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\\2019\\Community\\Common7\\Tools\\VsDevCmd.bat' + "&" + 'cmake . -B build' + "&" + 'cmake --build build --config RelWithDebInfo -j 16' )
 call(["python", "path.py"])
-call(["python", "run.py"])
