53dc17132cfb0fd2855bdcf1450eada28af59a34
Luka G <gostencnikluka@gmail.com>
2022-03-03 13:10:03 -0800

    Update path.py

--- path.py
+++ path.py
@@ -5,7 +5,7 @@ import shutil
 import ctypes
 from subprocess import call
 cwd = os.getcwd()
-pthf1 = 'setx /m PATH "', cwd, '\\ffmpeg\\bin;%PATH%"'
+pthf1 = 'setx PATH "', cwd, '\\ffmpeg\\bin;%PATH%"'
 pthf2 = str(pthf1)
 pthf3 = pthf2.removeprefix("('")
 pthf4 = pthf3.removesuffix("')")
@@ -15,14 +15,13 @@ pthc2 = str(pthc1)
 pthc3 = pthc2.removeprefix("('")
 pthc4 = pthc3.removesuffix("')")
 pthc5 = pthc4.replace("', '", "")
+os.system(pthf5)
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
 if is_admin():
-    os.system(pthf5)
-
     os.system(pthc5)
 else:
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
