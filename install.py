195bc9eb91e2d8d026e5c094363f6037c258fa4f
Luka G <gostencnikluka@gmail.com>
2022-04-04 13:09:40 -0700

    .

--- install.py
+++ install.py
@@ -1,8 +1,4 @@
 import os
-import subprocess
-import shutil
-import ctypes
-import sys
 from subprocess import call
 os.system('git clone --recursive https://github.com/nvlabs/instant-ngp' + "&" + 'cd instant-ngp' + "&" + 'C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\\2019\\Community\\Common7\\Tools\\VsDevCmd.bat' + "&" + 'cmake . -B build' + "&" + 'cmake --build build --config RelWithDebInfo -j 16' )
 call(["python", "copy.py"])
