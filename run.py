99ab7766c979cadc7cc8e8c910629b80b364acd2
Luka G <gostencnikluka@gmail.com>
2022-05-07 16:16:05 +0200

    The code no longer deletes the files
    The code no longer asks for permission to delete the files after its done executing

--- run.py
+++ run.py
@@ -18,16 +18,4 @@ os.system(command)
 os.chdir(path0)
 run = '"'+cwd+'\\instant-ngp\\build\\testbed" --scene tmp'
 os.system(run)
-x = input("Do you want to delete all files in instant-ngp\tmp folder [Y]es/[N]o: ")
-xu = x.upper()
-if xu == str('Y'):
-    folder = path4
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('failed to delete files')
+
