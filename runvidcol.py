3c11288969041cea172fe78116d4a8cd69c6a6a0
Luka G <gostencnikluka@gmail.com>
2022-05-19 14:25:28 -0700

    Update runvidcol.py

--- runvidcol.py
+++ runvidcol.py
@@ -1,14 +1,16 @@
 import os
 from os import listdir
 from os.path import isfile, join
+import run
 
 cwd = str(os.getcwd())
 path1 = cwd+'\\instant-ngp\\tmp'
 path0 = cwd+'\\instant-ngp'
+abab=run.aabb
 
 videos = [f for f in listdir(path1) if isfile(join(path1, f))]
 videostr = ''.join(videos)
 os.chdir(path1)
-command = 'python "'+path0+'\\scripts\\colmap2nerf.py" --video_in "'+videostr+'" --video_fps 2 --run_colmap --aabb_scale 16'
+command = 'python "'+path0+'\\scripts\\colmap2nerf.py" --video_in "'+videostr+'" --video_fps 2 --run_colmap --aabb_scale ' +abab
 os.system(command)
 print("done")
