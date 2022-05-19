3027f39ebb0ab0fd268dc8681955f80fc8a34445
Luka G <gostencnikluka@gmail.com>
2022-05-19 16:10:14 -0700

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 import os
 from subprocess import call
+os.system("pip install numpy")
+os.system("pip install opencv-python")
 os.system('git clone --recursive https://github.com/nvlabs/instant-ngp' + "&" + 'cd instant-ngp' + "&" + 'C:\\"Program Files (x86)"\\"Microsoft Visual Studio"\\2019\\Community\\Common7\\Tools\\VsDevCmd.bat' + "&" + 'cmake . -B build' + "&" + 'cmake --build build --config RelWithDebInfo -j 16' )
 call(["python", "copy.py"])
 call(["python", "path.py"])
