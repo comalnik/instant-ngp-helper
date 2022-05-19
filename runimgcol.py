94b9d22b553266fbb7519ee266e5e7a6db7103dd
Luka G <gostencnikluka@gmail.com>
2022-05-19 14:42:43 -0700

    Update runimgcol.py

--- runimgcol.py
+++ runimgcol.py
@@ -1,13 +1,11 @@
 import os
-import run
 
 cwd = str(os.getcwd())
 path1 = cwd+'\\instant-ngp\\tmp'
 path0 = cwd+'\\instant-ngp'
 imgstr = cwd+'\\instant-ngp\\tmp\\images'
-abab=run.aabb
 
 os.chdir(path1)
-command = 'python "'+path0+'\\scripts\\colmap2nerf.py" --colmap_matcher exhaustive --run_colmap --aabb_scale ' +abab
+command = 'python "'+path0+'\\scripts\\colmap2nerf.py" --colmap_matcher exhaustive --run_colmap --aabb_scale 16'
 os.system(command)
 print("done")
