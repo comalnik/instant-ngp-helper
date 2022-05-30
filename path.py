357742589881cc2425711461542aae22bad0adf8
Luka G <gostencnikluka@gmail.com>
2022-05-30 14:17:02 -0700

    Update path.py

--- path.py
+++ path.py
@@ -3,10 +3,8 @@ import sys
 import ctypes
 from subprocess import call
 cwd = str(os.getcwd())
-ffmpeg = 'setx PATH "'+cwd+'\\ffmpeg\\bin;%PATH%"'
 colmap = 'setx /m PATH "'+cwd+'\\COLMAP;%PATH%"'
 tmp = cwd+'\\instant-ngp\\tmp'
-os.system(ffmpeg)
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
